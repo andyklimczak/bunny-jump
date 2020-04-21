34d07d2eac14d7ac3fae6b1f5ac9330a0ce0f479
Andy Klimczak <andyklimczak@fastmail.com>
2020-04-20 22:20:09 -0400

    Restart text

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -15,6 +15,7 @@ export default class GameOver extends Phaser.Scene {
         const height = this.scale.height
 
         this.add.text(width * 0.5, height * 0.5, 'Game Over', {fontSize: 48}).setOrigin(0.5)
+        this.add.text(width * 0.5, height * 0.6, '(Press space)', {fontSize: 32}).setOrigin(0.5)
         this.sound.play('lowDown')
 
         this.input.keyboard.once('keydown_SPACE', () => {
